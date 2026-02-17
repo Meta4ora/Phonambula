468c7832a567add7b540324670d85d8d8ce77ed5
Denis Fokin <fokindenlelush2006@gmail.com>
2026-02-17 20:22:16 +0300

    Создание базовой структуры приложения. Интеграция технологии PWA. Добавление иконки

---
+++ public/service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "phonebook-cache-v1";
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/login.html",
+        "/admin.html",
+        "/manifest.json",
+        "/icons/icon-192.png",
+        "/icons/icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
